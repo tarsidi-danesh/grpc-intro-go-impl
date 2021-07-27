4e9e0597cfc9d2bb127e427b9152dee57e1092a1
Tarsidi <tarsidi.danesh@tiket.com>
2021-07-27 19:59:37 +0700

    add client

---
+++ client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	proto "github.com/tarsidi-danesh/grpc-intro-go/grpc/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+
+	con, err := grpc.Dial("localhost:60707", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+
+	defer con.Close()
+
+	client := proto.NewGreeterClient(con)
+
+	req := &proto.HelloRequest{
+		Name: "Tarsidi",
+	}
+
+	res, err := client.SayHello(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("error while calling SayHello RPC: %v", err)
+	}
+
+	log.Printf("Response from server: %v", res.Message)
+}
---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	proto "github.com/tarsidi-danesh/grpc-intro-go/grpc/proto"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (*server) SayHello(ctx context.Context, req *proto.HelloRequest) (*proto.HelloReply, error) {
+	requestName := "Hello " + req.Name
+
+	response := proto.HelloReply{
+		Message: requestName,
+	}
+
+	return &response, nil
+}
+
+func main() {
+	fmt.Println("GRpc server is running")
+
+	list, err := net.Listen("tcp", "0.0.0.0:60606")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	proto.RegisterGreeterServer(s, &server{})
+
+	if err := s.Serve(list); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
+}
